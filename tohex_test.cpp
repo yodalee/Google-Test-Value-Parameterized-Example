7309ea938c76bf5f1871c0a21c291736123ee476
Yoda Lee <lc85301@gmail.com>
2022-11-05 14:42:37 +0800

    Add fixture example

--- tohex_test.cpp
+++ tohex_test.cpp
@@ -1,8 +1,21 @@
 
 #include "tohex.h"
 #include <gtest/gtest.h>
+#include <iostream>
 
-TEST(ToHexTestSuite, zero) {
+TEST(ToHexTest, zero) {
   auto s = to_hex(0);
   EXPECT_EQ(s, "0x00000000");
+}
+
+class ToHexTestSuite : public ::testing::Test {
+protected:
+  void SetUp() override { std::cout << "Before Test" << std::endl; }
+  void TearDown() override { std::cout << "After Test" << std::endl; }
+};
+
+TEST_F(ToHexTestSuite, a5) {
+  auto s = to_hex(0xa5a55a5a);
+  EXPECT_EQ(s, "0xA5A55A5A");
+  std::cout << s << std::endl;
 }
