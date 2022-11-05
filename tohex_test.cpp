ca9a67f3b4b60285a8b6331a0198f3f6bf43a41f
Yoda Lee <lc85301@gmail.com>
2022-11-05 15:05:39 +0800

    Add parameterized test

--- tohex_test.cpp
+++ tohex_test.cpp
@@ -19,3 +19,21 @@ TEST_F(ToHexTestSuite, a5) {
   EXPECT_EQ(s, "0xA5A55A5A");
   std::cout << s << std::endl;
 }
+
+struct ToHexTestdata {
+  uint32_t in;
+  std::string ans;
+};
+
+class ToHexParameterSuite : public ::testing::TestWithParam<ToHexTestdata> {};
+
+TEST_P(ToHexParameterSuite, ParameterTest) {
+  auto param = GetParam();
+  auto s = to_hex(param.in);
+  EXPECT_EQ(s, param.ans);
+}
+
+INSTANTIATE_TEST_SUITE_P(TableInstantiation, ToHexParameterSuite,
+                         ::testing::Values(ToHexTestdata{1, "0x00000001"},
+                                           ToHexTestdata{0xffffffff,
+                                                         "0xFFFFFFFF"}));
